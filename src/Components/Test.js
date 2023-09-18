b37058e138cd35b6ef65e7845631a8409955713d
Abhishek Suryavanshi <abhisheksuryavanshi2308@gmail.com>
2023-09-18 13:10:47 +0530

    Learning Redux and Redux Toolkit

--- src/Components/Test.js
+++ src/Components/Test.js
@@ -1,18 +1,20 @@
 import React from 'react'
-import { increment, decrement, incrementBy25 } from '../Redux/Counter';
+import { increment, decrement, incrementBy25, add20 } from '../Redux/Counter';
 import { useDispatch, useSelector } from 'react-redux';
 
 const Test = () => {
   // console.log(useSelector((state) => state));
-  const { count } = useSelector((state) => state.mycounter);
+  const { count,products } = useSelector((state) => state.mycounter);
   const mydispatch = useDispatch();
 
   return (
     <div>
       {count}<br />
+      {products}<br />
       <button onClick={() => mydispatch(increment())}>Increment by 1</button>&nbsp;
       <button onClick={() => mydispatch(decrement())}>Decrement by 1</button>&nbsp;
       <button onClick={() => mydispatch(incrementBy25())}>increment by 25</button>
+      <button onClick={() => mydispatch(add20())}>Array</button>
     </div>
   )
 }
--- src/Redux/Counter.js
+++ src/Redux/Counter.js
@@ -3,7 +3,8 @@ import { createSlice } from '@reduxjs/toolkit'
 export const counterSlice = createSlice({
     name: 'counter',
     initialState: {
-        count: 10
+        count: 10,
+        products: [1, 2, 4, 5]
     },
     reducers: {
         increment: (state) => {
@@ -14,9 +15,12 @@ export const counterSlice = createSlice({
         },
         incrementBy25: (state) => {
             state.count += 25;
+        },
+        add20: (state) => {
+            state.products.shift();
         }
     }
 })
 
-export const { increment, decrement, incrementBy25 } = counterSlice.actions
+export const { increment, decrement, incrementBy25, add20 } = counterSlice.actions
 export default counterSlice.reducer
